aa6b2811730f262a4c542c00967d1e2e61525311
Nikolai Kottcov <nikolafgh@gmail.com>
2022-05-19 15:48:17 +0300

    upd

--- test_files/client.py
+++ test_files/client.py
@@ -2,20 +2,26 @@ import socket
 import time
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("127.0.0.1", 33343))
+server.bind(("10.0.0.2", 11115))
 server.listen()
-server.settimeout(5)
+server.settimeout(50)
 
 
 while True:
     usr, adr = server.accept()
     # usr.settimeout(20)
+    msg = usr.recv(1024)
+    if not msg:
+        break
+    print(msg)
+    usr.send(b"#AL#1\r\n")
     while True:
         try:
-            msg = usr.recv(1024)
+            msg = usr.recv(100000)
             if not msg:
                 break
             print(msg)
+            usr.send(b"#AD#1\r\n")
         except socket.timeout:
             print("in usr timeout triggered")
         # usr.send("#AL#1\r\n".encode("utf-8"))
